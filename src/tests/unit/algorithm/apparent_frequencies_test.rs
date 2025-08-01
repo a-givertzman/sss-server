82bc1521e4057143fb5f9fd2b1c544f2f1b03c19
Klaimmoro <klaimmor228@gmail.com>
2025-08-01 15:01:45 +0300

    fixing code

--- src/tests/unit/algorithm/apparent_frequencies_test.rs
+++ src/tests/unit/algorithm/apparent_frequencies_test.rs
@@ -13,7 +13,11 @@ use crate::{
     algorithm::{
         context::context_access::ContextRead, 
         eval::{
-            ApparentFrequenciesCtx, ApparentFrequenciesEval, PeriodExcitementCtx, RollingFrequencyCtx, RollingPeriodCtx, RollingPeriodEval, VesselMaxSpeedCtx, Zg
+            ApparentFrequenciesCtx,
+            ApparentFrequenciesEval, 
+            PeriodExcitementCtx, 
+            VesselMaxSpeedCtx, 
+            Zg
         }
     }, 
     kernel::{
@@ -43,12 +47,12 @@ fn init_each() -> () {}
 ///
 /// Testing 'eval'
 #[test]
-fn eval() {
+fn apparent_frequencies() {
     DebugSession::init(LogLevel::Info, Backtrace::Short);
     init_once();
     init_each();
     log::debug!("");
-    let dbg = "eval";
+    let dbg = "ApparentFrequencies";
     log::debug!("\n{}", dbg);
     let test_duration = TestDuration::new(dbg, Duration::from_secs(1));
     test_duration.run().unwrap();
