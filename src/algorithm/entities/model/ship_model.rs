a47dca40e9a854af619fdb8661febbb6c6c39cc3
KnstAnt <knstantna@gmail.com>
2025-08-11 16:15:11 +0300

    Back | init area_cache

--- src/algorithm/entities/model/ship_model.rs
+++ src/algorithm/entities/model/ship_model.rs
@@ -44,14 +44,16 @@ pub struct ShipModel {
     //   model_shape: Shape,
     ///
     /// Provides a number of calculations:
-    /// - cashe for model, [heel, trim, draught, volume, x, y, z, area, x, y, z, l_x, l_y, i_x, i_y ]
+    /// - cache for model, [heel, trim, draught, volume, x, y, z, area, x, y, z, l_x, l_y, i_x, i_y ]
     model: DisplacementCache,
-    /// - cashe for compartments, [index of compartments, [heel, trim, level, volume, x, y, z, i_x, i_y ]]
+    /// - cache for compartments, [index of compartments, [heel, trim, level, volume, x, y, z, i_x, i_y ]]
     compartments: IndexMap<usize, CompartmentCache>,
-    /// - cashe for bounds of model, [index of bound, [trim, draught, volume ]]
+    /// - cache for bounds of model, [index of bound, [trim, draught, volume ]]
     model_bounded: IndexMap<usize, Vec<BoundCache>>,    
-    /// - cashe for bounds of compartments,  [index of bound, TODO]
+    /// - cache for bounds of compartments,  [index of bound, TODO]
     compartments_bounded: IndexMap<usize, IndexMap<usize, IndexMap<usize, BoundCache>>>,
+    /// - cache for vertical area
+    vertical_area: AreaCache,
     scheduler: Scheduler,
 }
 //
