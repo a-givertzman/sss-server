ef8dc37fc83c31fad523711082865b43104f5b05
KnstAnt <knstantna@gmail.com>
2025-08-27 11:23:21 +0300

    Back | update computing for floating position

--- src/algorithm/entities/model_cached/model_cached.rs
+++ src/algorithm/entities/model_cached/model_cached.rs
@@ -493,13 +493,13 @@ impl ModelCached {
                     .fold(
                         (0., Moment::zero()),
                         |(mass_sum, moment_sum), (mass, moment)| {
-                            ((mass_sum + mass, moment_sum + moment))
+                            (mass_sum + mass, moment_sum + moment)
                         },
                     )
             };
             let mass_sum = mass_const + mass_bulk + mass_liquid + mass_damaged_compartment;
             // считаем корпус
-            let (draught, volume, cb) = {
+            let (new_draught, volume, cb) = {
                 // Prepare values (key) to extract data from `self.cache`.
                 // Note that 3rd parameter sets to None (as well as 5th and the rest).
                 // This means we expect to get their approximated values from the cache.
@@ -523,6 +523,7 @@ impl ModelCached {
                     Position::new(result[4], result[5], result[6]),
                 )
             };
+            draught = new_draught;
 
             let cg = {
                 let moment_sum =
@@ -530,13 +531,13 @@ impl ModelCached {
                 moment_sum.to_pos(mass_sum)
             };
 
-                let heel_rad = -heel.to_radians();
-                let trim_rad = trim.to_radians();
-                let trim_rotation = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), trim_rad);
-                let transformed_x_axis = trim_rotation.transform_vector(&Vector3::x_axis());
-                let transformed_x_axis = UnitVector3::new_normalize(transformed_x_axis);
-                let heel_rotation = UnitQuaternion::from_axis_angle(&transformed_x_axis, heel_rad);
-                let rotation = heel_rotation * trim_rotation;
+            let heel_rad = -heel.to_radians();
+            let trim_rad = trim.to_radians();
+            let trim_rotation = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), trim_rad);
+            let transformed_x_axis = trim_rotation.transform_vector(&Vector3::x_axis());
+            let transformed_x_axis = UnitVector3::new_normalize(transformed_x_axis);
+            let heel_rotation = UnitQuaternion::from_axis_angle(&transformed_x_axis, heel_rad);
+            let rotation = heel_rotation * trim_rotation;
 
             // Определение невязки
             let cg_h = {
@@ -583,166 +584,8 @@ impl ModelCached {
             };
 
             let frac_delta_psi_2 = ((cg_h - cg)/(cb_v - cg)).acos();
-            let frac_delta_theta_2 = 
+            let frac_delta_theta_2 = TODO;
 
-
-
-                let cg = Point::from(self.disp_center.point());
-                let size = self.centreline.len();
-                let v_plane_normal = self.centreline.dir().cross(&Vector::unit_z());
-                let v_plane =
-                    Face::rect(&self.disp_center, &v_plane_normal, 0.5 * size, 1.5 * size);
-                let frac_delta_psi_2 = 0.5 * {
-                    let cb_v = v_plane
-                        .project(&cb /* ~ CB */)
-                        .map(|vertex| Point::from(vertex.point()))
-                        .map_err(|err| {
-                            error.pass_with("cb_v = m_plane.project", err.to_string())
-                        })?;
-                    let cg_h = {
-                        let [.., z] = *cb_v;
-                        let [x, y, ..] = self.disp_center.point();
-                        Point::from(Vertex::<Attr>::new([x, y, z]).point())
-                    };
-                    //           ^
-                    //           Z
-                    //           |
-                    //     +--+--+
-                    //     |4 |1 |  \
-                    //     +--#--+   > part of vertical plane, where # points to CG,
-                    //     |3 |2 |  /  split into possible sections
-                    // <---+--+--+
-                    //  \
-                    //   centreline direction projected on horizontal plane
-                    //
-                    // If _delta psi_ is located in 1 or 3 section (see pic),
-                    // its value becomes positive (by defenition of trim).
-                    //
-                    // Example for section 3:
-                    //    .... # - CG
-                    //    .   /|
-                    //    .  /=|-- delta psi
-                    //    . /  |
-                    //    ./   |
-                    // <--#----# - CG_H
-                    // |   \
-                    // |    CB_V
-                    // |
-                    //  centreline direction projected on horizontal plane
-                    //
-                    let sign = {
-                        let [section_1, section_3] = {
-                            let [.., cg_z] = *cg;
-                            let [cb_v_x, ..] = *cb_v;
-                            let [cg_h_x, _, cg_h_z] = *cg_h;
-                            [
-                                cb_v_x >= cg_h_x && cg_h_z >= cg_z,
-                                cb_v_x <= cg_h_x && cg_h_z <= cg_z,
-                            ]
-                        };
-                        if section_1 || section_3 { 1.0 } else { -1.0 }
-                    };
-                    // calculate 'delta psi' (see schema in algorithm docs)
-                    // taking into account its section (see `sign` comments)
-                    sign * Vector::from([cg, cb_v]).angle(&Vector::from([cg, cg_h]))
-                };
-                let m_plane_normal = self.middle.normal_at(&self.middle.center());
-                let frac_delta_theta_2 = 0.5 * {
-                    let m_plane = {
-                        let size = 0.5 * size;
-                        Face::rect(&self.disp_center, &m_plane_normal, size, size)
-                    };
-                    let intersection = v_plane
-                        .intersect(&m_plane, OpConf { parallel: true })
-                        .edges()
-                        .into_iter()
-                        .next()
-                        .as_ref()
-                        .map(Edge::dir)
-                        .ok_or(format!(
-                            "{} | No intersection between Vertical\
-                                plane and Parallel to Midlle planes.",
-                            &self.dbg
-                        ))?;
-                    let cb_m = m_plane
-                        .project(&cb)
-                        .map(|vertex| Point::from(vertex.point()))
-                        .map_err(|err| {
-                            error.pass_with("cb_m = m_plane.project", err.to_string())
-                        })?;
-                    // Consider the tail of the model is behind the drawn part, then:
-                    //      |
-                    //   +--+--+
-                    //   |4 |1 |  \
-                    //   +--#--+   > part of plane parallel to `self.middle`,
-                    //   |3 |2 |  /  where # points to CG, split into possible sections
-                    // --+--+--+--
-                    // |    |
-                    // |     edge, which is the result of plane parallel to `self.middle`
-                    // |     and _vertical_ plane intersection
-                    // |
-                    //  edge, which is the result of plane parallel to `self.middle`
-                    //  and _horizontal_ plane intersection
-                    //
-                    // If _delta theta_ is located in 2 or 4 section (see pic),
-                    // its value becomes negative (by defenition of heel).
-                    //
-                    // Example for section 2:
-                    // CG - #.....
-                    //      |\   .
-                    //      |=\--.-- delta theta
-                    //      |  \ .
-                    //      |   \.
-                    //    --#----#-- - edge, which is the result of plane parallel to `self.middle`
-                    //     /    /      and _horizontal_ plane intersection
-                    //    /     CB_M
-                    //   /
-                    //  point of vertical plane, horizontal plane,
-                    //  and plane parallel to `self.middle` intersection
-                    //
-                    let sign = {
-                        let [section_2, section_4] = {
-                            let [.., cg_y, cg_z] = *cg;
-                            let [.., cb_m_y, cb_m_z] = *cb_m;
-                            [
-                                cg_y >= cb_m_y && cg_z >= cb_m_z,
-                                cg_y <= cb_m_y && cg_z <= cb_m_z,
-                            ]
-                        };
-                        if section_2 || section_4 { -1.0 } else { 1.0 }
-                    };
-                    // calculate 'delta psi' (see schema in algorithm docs)
-                    // taking into account the actual section (see `sign` comments)
-                    sign * Vector::from([cg, cb_m]).angle(&intersection)
-                };
-                // apply rotations to `self.middle` and `self.centreline`
-                {
-                    let cg = Vertex::new(*cg);
-                    self.middle = self
-                        .middle
-                        .rotate(cg.clone(), v_plane_normal, frac_delta_psi_2);
-                    self.centreline = self
-                        .centreline
-                        .rotate(cg.clone(), v_plane_normal, frac_delta_psi_2)
-                        .rotate(cg, m_plane_normal, frac_delta_theta_2);
-                }
-                [frac_delta_psi_2, frac_delta_theta_2]
-            };
-            // Align the current keel point vertiacally to its initial position.
-            // This is necessary to get correct results from cache on next iterations.
-            {
-                let dir = {
-                    let cur_keel = Point::from(self.centreline.center().point());
-                    let new_keel = {
-                        let [x, y, ..] = init_center;
-                        let [.., z] = *cur_keel;
-                        Point::from([x, y, z])
-                    };
-                    Vector::from([cur_keel, new_keel])
-                };
-                self.middle = self.middle.translate(dir);
-                self.centreline = self.centreline.translate(dir);
-            }
             heel += frac_delta_theta_2;
             trim += frac_delta_psi_2;
         }
