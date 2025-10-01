56d4a3695bbbf052d90145ceebe84301bf78827b
KnstAnt <knstantna@gmail.com>
2025-10-01 17:10:18 +0300

    Back | update mass

--- src/algorithm/eval/mass_eval/mass_eval.rs
+++ src/algorithm/eval/mass_eval/mass_eval.rs
@@ -1,8 +1,9 @@
 use super::mass_ctx::MassCtx;
 use crate::{
     algorithm::{
-        context::context_access::ContextReadRef, entities::data::loads::{AssignmentType, UnitCargoType},
-        eval::{IcingCtx, WettingCtx, parameters::ParameterID},
+        context::context_access::ContextReadRef,
+        entities::data::loads::{AssignmentType, UnitCargoType},
+        eval::{BalanceCtx, IcingCtx, WettingCtx, parameters::ParameterID},
     },
     kernel::{eval::Eval, types::eval_result::EvalResult},
     prelude::{ContextParamsWrite, ContextRead, ContextWrite, InitialCtx},
@@ -42,7 +43,28 @@ impl Eval<(), EvalResult> for MassEval {
                     .ship_parameters
                     .as_ref()
                     .ok_or(error.err("ship_parameters error: no data!"))?;
-                let gaseous = initial
+                let bounds = initial
+                    .bounds
+                    .as_ref()
+                    .ok_or(error.err("initial error: no bounds!"))?;
+                let unit = initial
+                    .unit
+                    .as_ref()
+                    .ok_or(error.err("Read unit error: no data!"))?
+                    .iter()
+                    .filter(|v| v.mass > 0.)
+                    .collect::<Vec<_>>();
+                let (unit, bulkhead): (Vec<_>, Vec<_>) = unit
+                    .into_iter()
+                    .partition(|v| v.cargo_type != UnitCargoType::GrainBulkhead);
+                let lightship = initial
+                    .load_constant
+                    .as_ref()
+                    .ok_or(error.err("Read lightship error: no data!"))?
+                    .data();
+                {
+                    // суммарные массы по типам
+                    let gaseous = initial
                         .gaseous
                         .as_ref()
                         .ok_or(error.err("Read gaseous error: no data!"))?
@@ -50,77 +72,119 @@ impl Eval<(), EvalResult> for MassEval {
                         .filter(|(_, v)| v.mass > 0.)
                         .map(|(_, v)| v)
                         .collect::<Vec<_>>();
-                let bulk = initial
+                    let bulk = initial
                         .bulk
                         .as_ref()
                         .ok_or(error.err("Read bulk error: no data!"))?
                         .iter()
                         .filter(|(_, v)| v.mass > 0.)
                         .map(|(_, v)| v)
-                        .collect::<Vec<_>>();  
-                let liquid = initial
+                        .collect::<Vec<_>>();
+                    let liquid = initial
                         .liquid
                         .as_ref()
                         .ok_or(error.err("Read liquid error: no data!"))?
                         .iter()
                         .filter(|(_, v)| v.mass > 0.)
                         .map(|(_, v)| v)
-                        .collect::<Vec<_>>();                   
-                let unit = initial
-                        .unit
+                        .collect::<Vec<_>>();
+                    let mass = |assigment_type: AssignmentType| {
+                        gaseous
+                            .iter()
+                            .filter(|v| v.assigment_type == assigment_type)
+                            .fold(0., |sum, v| sum + v.mass)
+                            + bulk
+                                .iter()
+                                .filter(|v| v.assigment_type == assigment_type)
+                                .fold(0., |sum, v| sum + v.mass)
+                            + liquid
+                                .iter()
+                                .filter(|v| v.assigment_type == assigment_type)
+                                .fold(0., |sum, v| sum + v.mass)
+                            + unit
+                                .iter()
+                                .filter(|v| v.assigment_type == assigment_type)
+                                .fold(0., |sum, v| sum + v.mass)
+                    };
+                    let ballast = mass(AssignmentType::Ballast);
+                    let stores = mass(AssignmentType::Stores);
+                    let cargo = mass(AssignmentType::CargoLoad);
+                    let bulkhead = bulkhead.iter().fold(0., |sum, v| sum + v.mass);
+                    let deadweight = ballast + stores + cargo + bulkhead; // Суммарная масса переменного груза
+                    let lightship = lightship.iter().fold(0., |sum, v| sum + v.mass);
+                    let icing = ContextRead::<IcingCtx>::read(&ctx).mass;
+                    let wetting = ContextRead::<WettingCtx>::read(&ctx).mass;
+                    let mass_sum = deadweight + lightship + wetting + icing;
+                    ctx.write_params(ParameterID::Displacement, mass_sum);
+                    ctx.write_params(ParameterID::MassBallast, ballast);
+                    ctx.write_params(ParameterID::MassStores, stores);
+                    ctx.write_params(ParameterID::MassBulkhead, bulkhead);
+                    ctx.write_params(ParameterID::MassCargo, cargo);
+                    ctx.write_params(ParameterID::MassDeadweight, deadweight);
+                    ctx.write_params(ParameterID::MassLightship, lightship);
+                    ctx.write_params(ParameterID::MassIcing, icing);
+                    ctx.write_params(ParameterID::MassWetting, wetting);
+                    log::info!(
+                        "\t Mass ballast:{ballast}, stores:{stores}, bulkhead:{bulkhead}
+                        cargo:{cargo}, deadweight:{deadweight}, lightship:{lightship},
+                        icing:{icing}, wetting:{wetting} sum:{mass_sum}"
+                    );
+                }
+                {
+                    // распределения масс по типам
+                    let balance: BalanceCtx = ctx.read();
+                    let gaseous = initial
+                        .gaseous
                         .as_ref()
-                        .ok_or(error.err("Read unit error: no data!"))?
-                        .iter()
-                        .filter(|v| v.mass > 0.)
-                        .collect::<Vec<_>>();  
-                let mass_type = |assigment_type: AssignmentType| {
-                    gaseous
-                        .iter()
-                        .filter(|v| v.assigment_type == assigment_type)
-                        .fold(0., |sum,v| sum + v.mass) + 
-                    bulk
-                        .iter()
-                        .filter(|v| v.assigment_type == assigment_type)
-                        .fold(0., |sum,v| sum + v.mass) +
-                    liquid
-                        .iter()
-                        .filter(|v| v.assigment_type == assigment_type)
-                        .fold(0., |sum,v| sum + v.mass) +
-                    unit
-                        .iter()
-                        .filter(|v| v.assigment_type == assigment_type && v.cargo_type != UnitCargoType::GrainBulkhead)
-                        .fold(0., |sum, v| sum + v.mass)                                             
-                };
-                let ballast = mass_type(AssignmentType::Ballast);
-                let stores = mass_type(AssignmentType::Stores);
-                let cargo = mass_type(AssignmentType::CargoLoad);
-                let bulkhead = unit
-                        .iter()
-                        .filter(|v| v.cargo_type == UnitCargoType::GrainBulkhead)
-                        .fold(0., |sum,v| sum + v.mass);              
-                let deadweight = ballast + stores + cargo + bulkhead; // Суммарная масса переменного груза
-                let lightship = initial.load_constant.as_ref()
-                        .ok_or(error.err("Read liquid error: no data!"))?
-                        .data()
-                        .iter()
-                        .fold(0., |sum, v| sum + v.mass);             
-                let icing = ContextRead::<IcingCtx>::read(&ctx).mass;
-                let wetting = ContextRead::<WettingCtx>::read(&ctx).mass;
-                let mass_sum = deadweight + lightship + wetting + icing;
-                ctx.write_params(ParameterID::Displacement, mass_sum);
-                ctx.write_params(ParameterID::MassBallast, ballast);
-                ctx.write_params(ParameterID::MassStores, stores);
-                ctx.write_params(ParameterID::MassBulkhead, bulkhead);
-                ctx.write_params(ParameterID::MassCargo, cargo);
-                ctx.write_params(ParameterID::MassDeadweight, deadweight);
-                ctx.write_params(ParameterID::MassLightship, lightship);
-                ctx.write_params(ParameterID::MassIcing, icing);
-                ctx.write_params(ParameterID::MassWetting, wetting);
-                log::info!(
-                    "\t Mass ballast:{ballast}, stores:{stores}, bulkhead:{bulkhead}
-                    cargo:{cargo}, deadweight:{deadweight}, lightship:{lightship},
-                    icing:{icing}, wetting:{wetting} sum:{mass_sum}"
-                );
+                        .ok_or(error.err("Read gaseous error: no data!"))?;
+                    let bulk = initial
+                        .bulk
+                        .as_ref()
+                        .ok_or(error.err("Read bulk error: no data!"))?;
+                    let liquid = initial
+                        .liquid
+                        .as_ref()
+                        .ok_or(error.err("Read liquid error: no data!"))?;
+                    let mut vec_hull = Vec::new();
+                    let mut vec_equipment = Vec::new();
+                    let mut vec_bulkhead = Vec::new();
+                    let mut vec_ballast = Vec::new();
+                    let mut vec_store = Vec::new();
+                    let mut vec_cargo = Vec::new();
+                    let mut vec_icing = ContextRead::<IcingCtx>::read(&ctx).mass_values;
+                    let mut vec_wetting = ContextRead::<WettingCtx>::read(&ctx).mass_values;
+                    let mut vec_sum = Vec::new();
+                    let mut res: Vec<f64> = Vec::new();
+                    for b in bounds.iter() {
+                        vec_hull.push(
+                            lightship
+                                .iter()
+                                .fold(0., |s, v| s + v.mass(b).unwrap_or(0.)),
+                        );
+                        vec_equipment.push(0.); //    TODO - сейчас в базе нет данных по equipment 
+                        vec_bulkhead
+                            .push(bulkhead.iter().fold(0., |s, v| s + v.mass(b).unwrap_or(0.)));
+                        vec_ballast
+                            .push(unit.iter().filter(|v| v.assigment_type == AssignmentType::Ballast).fold(0., |s, v| s + v.mass(b).unwrap_or(0.)));
+                        vec_store
+                            .push(unit.iter().filter(|v| v.assigment_type == AssignmentType::Stores).fold(0., |s, v| s + v.mass(b).unwrap_or(0.)));
+                        vec_cargo
+                            .push(unit.iter().filter(|v| v.assigment_type == AssignmentType::CargoLoad).fold(0., |s, v| s + v.mass(b).unwrap_or(0.)));
+                    }
+
+                    for v in balance.gaseous {
+
+                    }
+
+                    
+                                    let mut vec_ballast = Vec::new();
+                    let mut vec_store = Vec::new();
+                    let mut vec_cargo = Vec::new();
+
+                                        res.push(
+                            hull + equipment + bulkhead + ballast + store + cargo + icing + wetting,
+                        );                    
+                }
 
 
                 let result = MassCtx::new(data, values);
