3a8bf84699780da29c681b8329e9eaa540843151
KnstAnt <knstantna@gmail.com>
2025-10-17 12:55:00 +0300

    Back | update caches

--- src/main.rs
+++ src/main.rs
@@ -36,42 +36,8 @@ use std::{collections::HashMap, path::PathBuf};
 /// Application entry point
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //   DebugSession::init(LogLevel::Debug, Backtrace::Short);
-
-    /*
-        let dbg = Dbg::new("main", "bound_cache");
-        let cache_dir: PathBuf = "src/assets/cache/sofia".into();
-        let model_dir: PathBuf = "src/assets/model/sofia".into();
-        let model_center_coord = Position::new(65.250, 0., 0.);
-        let bounds = Bounds::from_n(138.86, model_center_coord.x(), 20).unwrap();
-        let bounds_length_mm = (bounds.length() * 1000.).ceil() as usize;
-        let thread_pool = ThreadPool::new(&dbg, Some(15));
-        let displacement_shape = Arc::new(RwLock::new(DisplacementShape::new_uninit(
-            &dbg,
-            model_dir.clone().join(PathBuf::from("hull.stl")),
-            Some(model_center_coord),
-            1000.,
-        )));
-        displacement_shape.write().init().unwrap();
-        dbg!("displacement_shape init ok");
-        let mut bound_cache = BoundDisplacementCache::new(
-            &dbg,
-            displacement_shape,
-            cache_dir
-                .clone()
-                .join("disp_bounded")
-                .join(format!("{bounds_length_mm}")),
-            1.,
-            bounds.clone(),
-            thread_pool.scheduler(),
-        );
-        dbg!("BoundCache::new ok");
-        let res = bound_cache.rebuild();
-        dbg!("BoundCache::new rebuild", res);
-        let res = bound_cache.get(5.9, 0.);
-        dbg!("BoundCache::new get", res);
-    */
     
- /*   let physical_frames = [
+    let physical_frames = [
         -3.6, -3.0, -2.4, -1.8, -1.2, -0.6, 0.0, 0.6, 1.2, 1.8, 2.4, 3.0, 3.6, 4.2, 4.8, 5.4, 6.0,
         6.7, 7.4, 8.1, 8.8, 9.5, 10.2, 10.9, 11.6, 12.3, 13.0, 13.7, 14.4, 15.1, 15.8, 16.5, 17.2,
         17.9, 18.6, 19.34, 20.08, 20.82, 21.56, 22.3, 23.04, 23.78, 24.52, 25.26, 26.0, 26.74,
@@ -118,9 +84,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 3., 5., 7.5, 10., 12.5, 20., 25., 30., 40.,
             ],
             ship_length_lbp: 130.5,
-            draught_min: 2.,
-            draught_max: 14.,
-            hull_draught_step: 1.,
+            draught_min: 0.5,
+            draught_max: 1.5,
+
+      //      draught_min: 2.,
+       //     draught_max: 14.,
+            hull_draught_step: 0.5,
             bounds_level_step: 0.1,
             compartment_level_step: 1.,
             compartment_data: HashMap::new(),
@@ -128,14 +97,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         thread_pool.scheduler(),
     )
     .unwrap();
-  //  let res = model.reload_shapes();   dbg!(&res);
-  //  let res = model.rebuild_caches();   dbg!(&res);
+    let res = model.reload_shapes();   dbg!(&res);
+    let res = model.rebuild_caches();   dbg!(&res);
   //  let res = model.rebuild_bounds(&bounds);   dbg!(&res);
     let res = model.init();
     dbg!(&res);
     let res = model.init_bounded(&bounds);
     dbg!(&res);
-*/
+
 
  /*   let mut result = |mass: f64, x: f64, y: f64, z: f64| {
               model.floating_position(model_cached::FloatingPositionQuery {
@@ -221,7 +190,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!(res);
     */
 
-    
+ /*   
     let dbg = Dbg::own("main");
     let tmp_dbg = dbg.clone();
     let path = "config.yaml";
@@ -437,5 +406,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .eval(());*/
     dbg!(ctx);
+    */
     Ok(())
 }
