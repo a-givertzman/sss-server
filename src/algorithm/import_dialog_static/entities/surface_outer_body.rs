4efd34764f8554bb0e2c5065474dc22581ce53cd
Klaimmoro <zakhar.vorzhev@yandex.ru>
2025-10-27 14:47:40 +0300

    adding new data

--- src/algorithm/import_dialog_static/entities/surface_outer_body.rs
+++ src/algorithm/import_dialog_static/entities/surface_outer_body.rs
@@ -1,5 +1,7 @@
+use indexmap::IndexMap;
+
 ///
 /// Store coordinates of surface outer body
 pub struct SurfaceOuterBody {
-    pub coordinates: (f64,f64) // (Z,X)
+    pub coordinates: IndexMap<f64,(f64,f64)>, // (X, (Z, Y))
 }
