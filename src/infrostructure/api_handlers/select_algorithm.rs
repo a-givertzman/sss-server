94af24270ebe31ea4b495b0ecf690f4dd65443b5
Anton Lobanov <a.givertzman@icloud.com>
2025-11-24 20:18:41 +0300

    Server | Algorithm

--- src/infrostructure/api_handlers/select_algorithm.rs
+++ src/infrostructure/api_handlers/select_algorithm.rs
@@ -1,7 +1,7 @@
 use std::{fmt::Debug, sync::{Arc, atomic::AtomicBool}};
 use sal_core::{dbg::Dbg, error::Error};
 use sal_sync::{sync::Handles, thread_pool::Scheduler};
-use crate::{conf::AlgorithmConf, kernel::{Eval, EvalEx, sync::Link, types::eval_result::EvalResult}, server::{self, AlgorithmQuery, AlgorithmReply, Query, Reply, Request, extract}};
+use crate::{conf::AlgorithmConf, kernel::{Eval, EvalEx, sync::Link, types::eval_result::EvalResult}, server::{self, AlgorithmQuery, AlgorithmReply, Event, Query, Reply, Request, extract}};
 
 ///
 /// Evaluates entair ship calculations in the separate thread
@@ -9,7 +9,7 @@ pub struct SelectAlgorithm {
     conf: AlgorithmConf,
     scheduler: Scheduler,
     handles: Handles<()>,
-    ctx: Box<dyn Eval<AlgorithmQuery, EvalResult> + Send + Sync>,
+    ctx: Arc<Box<dyn Eval<AlgorithmQuery, EvalResult> + Send + Sync>>,
     exit: Arc<AtomicBool>,
     dbg: Dbg,
 }
@@ -29,7 +29,7 @@ impl SelectAlgorithm {
             conf,
             scheduler,
             handles: Handles::new(&dbg),
-            ctx: Box::new(ctx),
+            ctx: Arc::new(Box::new(ctx)),
             exit: Arc::new(AtomicBool::new(false)),
             dbg,
         }
@@ -39,23 +39,35 @@ impl SelectAlgorithm {
 //
 impl<K: Debug + Copy + bincode::Encode + Send + 'static> EvalEx<(Request<K>, Option<Link>), server::EvalResult<K>> for SelectAlgorithm {
     fn eval(&self, (req, link): (Request<K>, Option<Link>)) -> server::EvalResult<K> {
-        let error = Error::new("SelectDevInfo", "eval");
-        let query = extract!(&req.query, Query::Algorithm)
+        let dbg = self.dbg.clone();
+        let error = Error::new(&dbg, "eval");
+        let link = link.ok_or(error.err("Can't get Link"))?;
+        let query = extract!(&req.query, Query::Algorithm).cloned()
             .map_err(|_| error.err(format!("Query::DeviceInfo expected, but found {:?}", req.query_id)))?;
         //
         // Do something woth incomong query...
         // let param1 = query.param1;
         // let param2 = query.param2;
         //
-        // Generate and return reply to the request
+        let ctx = self.ctx.clone();
+        let error1 = error.clone();
         let h = self.scheduler.spawn(move || {
-            match self.ctx.eval(query.clone()) {
+            //
+            // Generate and return reply to the request
+            let response = match ctx.eval(query.clone()) {
                 Ok(ctx) => {
-                    let response = req.reply(Reply::Algorithm(AlgorithmReply {}));
+                    //
+                    // Do required operations with the Context
+                    //
+                    // Then send reply
+                    req.reply(Reply::Algorithm(AlgorithmReply {}))
                 }
                 Err(err) => {
-                    let response = req.reply_err(error.pass_with("Calculations failed", err));
+                    req.reply_err(error1.pass_with("Calculations failed", err))
                 }
+            };
+            if let Err(err) = link.send(Event::from(&dbg, response)) {
+                log::warn!("{dbg}.eval | Can't send reply: {:?}", err);
             }
             Ok(())
         }).map_err(|err| error.pass_with("Can't spawn thread", err));
