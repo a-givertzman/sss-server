6b886294f62c89fe3b668970c74d27713a2b2eff
Konstantin Antonov <knstantna@gmail.com>
2026-04-06 16:11:52 +0300

    Back | fix dso

--- src/tests/unit/algorithm/full.rs
+++ src/tests/unit/algorithm/full.rs
@@ -51,6 +51,8 @@ fn full() -> Result<(), Box<dyn std::error::Error>> {
     let mut dso_angles = vec![-60., -50., -40., -30., -12., 12., 30., 40., 50., 60.];
     dso_angles.append(&mut ((-11..=11).map(|v| (v as f64) * 5.).collect())); // -55, -50 .. 55
     dso_angles.append(&mut ((-8..=8).map(|v| v as f64).collect()));
+
+    dso_angles.append(&mut ((-60..=60).map(|v| v as f64).collect()));    
     dso_angles.sort_by(|a, b| a.partial_cmp(&b).unwrap());
     dso_angles.dedup();    
     let model_cached = model_cached::ModelCached::new(
